2bde7883198b680d838c93fc337d50e5c334f7ce
Andrew Hill <awh19@pitt.edu>
2020-07-04 14:22:52 -0400

    Connecting DB to SQL

--- src/MysqlCon.java
+++ src/MysqlCon.java
@@ -3,11 +3,10 @@ import java.sql.*;
 class MysqlCon {
     public static void main(String args[]) {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://axxb6a0z2kydkco3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/nkfszrrxkyw5jypd", "z2hl7aklk6bs8bl0", "d397ug19mkb2lksa");
-            // here sonoo is database name, root is username and password
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("select * from emp");
+            ResultSet rs = stmt.executeQuery("select * from tbl_exercises");
             while (rs.next())
                 System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
             con.close();
