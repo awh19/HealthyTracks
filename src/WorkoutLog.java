d71a64fdb46a95741d79fb4ab35b837217c1fa27
Andrew Hill <awh19@pitt.edu>
2020-07-06 10:41:14 -0400

    Creating WorkoutLog object

---
+++ src/WorkoutLog.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
+public class WorkoutLog {
+    private ArrayList<Exercise> exerciseList;
+    private LocalDateTime currentDate;
+
+    public WorkoutLog(){
+        exerciseList = new ArrayList<>();
+        currentDate = this.getDate();
+    }
+
+    public void listPossibleExercises(){
+        //TODO: CONNECT TO DATABASE HERE AND display LIST OF EXERCISES
+        return;
+    }
+
+    public void AddExercise(Exercise e){
+        exerciseList.add(e);
+    }
+
+    public void removeExercise(Exercise e){
+        exerciseList.remove(e);
+    }
+
+    public void updateExercise(Exercise e){
+        //TODO: UPDATE METHOD TO HAVE LOGIC ON WHETHER OR NOT TO UPDATE SETS/REPS/WEIGHT
+        return;
+    }
+
+    public LocalDateTime getDate(){
+        return LocalDateTime.now();
+    }
+
+}
