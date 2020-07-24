68e22f5eab83c4ca3dda0a19833caee6d7f1baed
awn3 <59579081+awn3@users.noreply.github.com>
2020-07-24 13:27:29 -0400

    Add files via upload

--- profileConnector.java
+++ profileConnector.java
@@ -10,6 +10,7 @@ public class profileConnector {
     private Connection con;
     private Statement stmt;
     private ResultSet rs;
+    
     private ConnectionManager conMag = new ConnectionManager();
 
 
@@ -30,8 +31,19 @@ public class profileConnector {
     
     public boolean addProfile(Profile e){
         try {
+        	
+        	
+        	
+       
+        	stmt.executeUpdate("INSERT INTO tbl_test (`id`, `name`) VALUES ('2', 'bob');");
+
+        //	stmt.executeUpdate("INSERT INTO `tbl_login` (`userId`, `userName`, `password`, `firstName`, `lastName`, `weight`, `feet`, `inches`) VALUES ('3', 'adam3', 'pass3', 'bob', 'fred', '110', '61', '21');");
+        			              
+        	String query1 = "INSERT INTO tbl_login (`userName`, `password`, `firstName`, `lastName`, 'weight', 'feet', 'inches') VALUES ('+adamnash5+','+passs1+', '+adam+', '+nash+', '+100+', '+6+', '+2+');";
+         //   stmt.executeUpdate(query1);
+        	
             String query = "INSERT INTO tbl_login (`userName`, `password`, `firstName`, `lastName`, 'weight', 'feet', 'inches') VALUES ('"+e.getUsername()+"','"+e.getPassword()+"', '"+e.getFirstName()+"', '"+e.getLastName()+"', '"+e.getWeight()+"', '"+e.getFeet()+"', '"+e.getInches()+"');";
-            stmt.executeUpdate(query);
+         //   stmt.executeUpdate(query);
             return true;
         } catch (SQLException throwables) {
             throwables.printStackTrace();
