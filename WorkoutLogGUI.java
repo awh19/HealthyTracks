69c62a28b4db86d222cac610c40e6b5ecb289612
zht14 <1149283516z@gmail.com>
2020-07-27 14:33:05 -0400

    DISPOSE_ON_CLOSE

--- WorkoutLogGUI.java
+++ WorkoutLogGUI.java
@@ -54,7 +54,7 @@ public class WorkoutLogGUI extends javax.swing.JFrame {
 
 
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setBackground(new java.awt.Color(50, 58, 69));
         setResizable(false);
 
